53466cd46602d241a3031d15b04ad17c2d234132
josego85 <josego85@gmail.com>
2022-08-07 09:32:05 +0200

    [feat] Add home screen

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# v0.0.1
+
+- [feat] First commit. The app in react native.
+- [feat] Add home screen.
---
+++ app/screens/Home/Index.js
@@ -0,0 +1,39 @@
+/* eslint-disable react/jsx-filename-extension */
+import * as React from 'react';
+import { Text, StyleSheet, View, ImageBackground } from 'react-native';
+
+const styles = StyleSheet.create({
+    background: {
+        with: '100%',
+        height: '100%',
+        backgroundColor: '#465f7e',
+    },
+    view: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    text: {
+        color: 'white',
+        marginTop: 10,
+        fontSize: 14,
+    },
+});
+
+// eslint-disable-next-line no-unused-vars
+function HomeScreen(props) {
+    return (
+        <ImageBackground style={styles.background}>
+            <View style={styles.view}>
+                <Text style={styles.title}>Cushing</Text>
+            </View>
+        </ImageBackground>
+    );
+}
+
+export default HomeScreen;
